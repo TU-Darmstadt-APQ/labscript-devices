c6095c19f6aca9da8358d4d69f52fa5a8733603a
Marcel Mittenbühler <marcel@mittenbuehler.com>
2022-02-28 20:39:36 +0100

    Check which device is used for jumps from the connection table

--- labscript_devices/NI_DAQmx/blacs_tabs.py
+++ labscript_devices/NI_DAQmx/blacs_tabs.py
@@ -138,6 +138,8 @@ class NI_DAQmxTab(DeviceTab):
                 timeout_trigger_type = waits.attrs['wait_monitor_timeout_trigger_type']
             except KeyError:
                 timeout_trigger_type = 'rising'
+            jumps = f['jumps']
+            jump_device = jumps.attrs['jump_device']
 
         # Create and set the primary worker
         self.create_worker(
@@ -162,29 +164,30 @@ class NI_DAQmxTab(DeviceTab):
         )
         self.primary_worker = "main_worker"
 
-        
-        self.create_worker(
-            "jump_worker",
-            'labscript_devices.NI_DAQmx.blacs_workers.NI_DAQmxJumpWorker',
-            {
-                'MAX_name': self.MAX_name,
-                'Vmin': AO_base_min,
-                'Vmax': AO_base_max,
-                'num_AO': num_AO,
-                'ports': ports,
-                'devices': json.dumps([str(d) for d in self.settings["connection_table"].get_attached_devices().keys()]),
-                'clock_limit': clock_limit,
-                'clock_terminal': clock_terminal,
-                'clock_mirror_terminal': clock_mirror_terminal,
-                'static_AO': static_AO,
-                'static_DO': static_DO,
-                'DO_hardware_names': DO_hardware_names,
-                'wait_timeout_device': wait_timeout_device,
-                'wait_timeout_connection': wait_timeout_connection,
-                'wait_timeout_rearm_value': int(timeout_trigger_type == 'falling')
-            },
-        )
-        self.add_secondary_worker("jump_worker")
+
+        if jump_device == self.device_name:
+            self.create_worker(
+                "jump_worker",
+                'labscript_devices.NI_DAQmx.blacs_workers.NI_DAQmxJumpWorker',
+                {
+                    'MAX_name': self.MAX_name,
+                    'Vmin': AO_base_min,
+                    'Vmax': AO_base_max,
+                    'num_AO': num_AO,
+                    'ports': ports,
+                    'devices': json.dumps([str(d) for d in self.settings["connection_table"].get_attached_devices().keys()]),
+                    'clock_limit': clock_limit,
+                    'clock_terminal': clock_terminal,
+                    'clock_mirror_terminal': clock_mirror_terminal,
+                    'static_AO': static_AO,
+                    'static_DO': static_DO,
+                    'DO_hardware_names': DO_hardware_names,
+                    'wait_timeout_device': wait_timeout_device,
+                    'wait_timeout_connection': wait_timeout_connection,
+                    'wait_timeout_rearm_value': int(timeout_trigger_type == 'falling')
+                },
+            )
+            self.add_secondary_worker("jump_worker")
 
         if wait_acq_device == self.device_name:
             if wait_timeout_device:
