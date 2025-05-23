deadadd195c6f7f15731e17d8a7d2ab67eb7fc24
ATOMICS-Lab <atomicslab@physik.tu-darmstadt.de>
2025-05-23 11:01:29 +0200

    Initial commit

---
+++ labscript_devices/HV_stahl/HV_stahl.md
@@ -0,0 +1,7 @@
+# Stahl HV- Series
+Voltage source for Spectroscopy Trap (ST)
+
+Interface: USB (9600)
+Output: BNC
+
+Supply of stable DC voltages for ion traps
---
+++ labscript_devices/HV_stahl/register_classes.py
@@ -0,0 +1,7 @@
+from labscript_devices import register_classes
+
+register_classes(
+    "HV_stahl",
+    BLACS_tab='user_devices.HV_stahl.BLACS_tabs.HV_Tab',
+    runviewer_parser=None,
+)
