b97065881c46419c98fa86d632cc2f6ce7a03713
Kerim Takouti <kerim.takouti@stud.tu-darmstadt.de>
2025-06-25 10:05:28 +0200

    added real time status tracking functionality

--- labscript_devices/HP_6622A.py
+++ labscript_devices/HP_6622A.py
@@ -10,6 +10,8 @@ import h5py
 from blacs.tab_base_classes import define_state
 from blacs.tab_base_classes import MODE_MANUAL, MODE_TRANSITION_TO_BUFFERED, MODE_TRANSITION_TO_MANUAL, MODE_BUFFERED
 from blacs.device_base_class import DeviceTab
+from PyQt5.QtWidgets import QLabel,QWidget,QHBoxLayout
+from PyQt5.QtCore import Qt
 
 # --- Worker Imports
 from labscript_devices.GPIBDevice import GPIBWorker
@@ -127,8 +129,6 @@ class HP_6622A(IntermediateDevice):
                 else: raise TypeError(device)
 
                 channel_no, output_type = device.connection.replace('out', '').split('/')
-                logger.info(f"output_type {output_type}")
-                logger.info(f"device {type(device)}")
                 if not self._check_output( device , output_type):
                     raise LabscriptError(f"The {output_type} specified for {device.connection} is not within the power supply's {output_type} range")
 
@@ -153,12 +153,19 @@ class HP_6622A(IntermediateDevice):
 class HP_6622ATab(DeviceTab):
 
     def initialise_GUI(self):
-        # Pull the following information out of the connection table:
+        # --- This allows to register a function to be called every periode of time t (ms)
+        time_check_state = 5        # in seconds
+        time_check_state = time_check_state * 1e3 # conversion to ms
+        self.statemachine_timeout_add(time_check_state ,self.status_monitor)
+
+        # --- Pull the following information out of the connection table:
         connection_table = self.settings['connection_table']
         connection_table_properties = connection_table.find_by_name(self.device_name).properties
         self.num_outputs = int(connection_table_properties['num_outputs'])
+        connection_table_entry = self.settings['connection_table'].find_by_name(self.settings['device_name'])
+        self.GPIB_address = connection_table_entry.BLACS_connection
 
-        # Capabilities:
+        # --- Capabilities:
         self.base_units = {'v': 'V', 'c': 'A'}
         self.base_step = {'v': 0.1, 'c': 0.01}  # step size for +/- buttons
         self.base_decimals = {'v': voltage_decimals, 'c': current_decimals}  # display accuracy
@@ -178,15 +185,49 @@ class HP_6622ATab(DeviceTab):
                                                             'decimals': self.base_decimals['c']
                                                             }
 
+        # -------------------------------------------------- Widgets
+        # --- Auto widgets
         self.create_analog_outputs(analog_properties)
-
         _, ao_widgets, _ = self.auto_create_widgets()
-        # logger.info(f"{ao_widgets}")
+
+        # --- Add The readback blacs widgets
+        val = 1
+        self.readback_widgets = {}
+        for key,widget in ao_widgets.items():
+            widget_layout = widget.layout()     # which is a GridLayout Btw
+            readback_widget = QWidget()
+            readback_layout = QHBoxLayout()
+            readback_layout.addWidget(QLabel(f"Readback: {val}"))
+            readback_widget.setLayout(readback_layout)
+            widget_layout.addWidget(readback_widget,2,0,alignment=Qt.AlignmentFlag.AlignTop)   # row # column
 
         self.auto_place_widgets(ao_widgets)
 
-        connection_table_entry = self.settings['connection_table'].find_by_name(self.settings['device_name'])
-        self.GPIB_address = connection_table_entry.BLACS_connection
+        # --- Create status a status label
+        self.status_labels_dict = {} 
+        layout = self.get_tab_layout()
+        layout.setSpacing(5) 
+        layout.setContentsMargins(5, 5, 5, 5)
+        for i in range(1,self.num_outputs +1):
+            self.status_label = QLabel(f"Mode channel {i} : Unknown")
+            self.status_labels_dict[i] = self.status_label
+            layout.addWidget(self.status_label,alignment=Qt.AlignmentFlag.AlignTop)
+
+        # --- Another thing that can be done, but not adequate here
+        # self.supports_remote_value_check(True) # This one checks the precise values and asks the user which one to choose
+
+    @define_state(MODE_MANUAL,True,delete_stale_states=True)
+    def program_device(self):
+        super().program_device()    # To don't disturb the basic functionalities
+        current_output_values = yield(self.queue_work(self.primary_worker,'get_readbcks'))
+        logger.info(current_output_values)
+
+    @define_state(MODE_MANUAL, True)
+    def status_monitor(self):
+        for chan in range(1,self.num_outputs +1):
+            status_label = self.status_labels_dict[chan] 
+            mode = yield (self.queue_work(self.primary_worker, "check_status",chan))
+            status_label.setText(f"Mode channel {chan} : {mode}")
 
 
     @define_state(MODE_MANUAL, True)
@@ -222,6 +263,7 @@ class HP_6622AWorker(GPIBWorker):
             raise LabscriptError("Failed setting voltage : ",e)
 
     def get_v(self,chan):
+        # Accuracy of the readback value over an interface is the same as the analog-to-digital converter
         try:
             sendStr = f"VOUT? {chan}" 
             return self.GPIB_connection.query(sendStr)
@@ -236,15 +278,23 @@ class HP_6622AWorker(GPIBWorker):
             raise LabscriptError(f"Failed setting current: {e}")
 
     def get_i(self, chan):
+        # Accuracy of the readback value over an interface is the same as the analog-to-digital converter
         try:
             sendStr = f"IOUT? {chan}"
             return self.GPIB_connection.query(sendStr)
         except Exception as e:
             raise LabscriptError(f"Failed getting current: {e}")
-
+        
+    def get_mode(self,chan):
+        try:
+            sendStr = f"ASTS? {chan}"
+            result = self.GPIB_connection.query(sendStr)
+            return int(result)
+        except Exception as e:
+            raise LabscriptError(f"Failed getting mode: {e}")
     # -------------------------- Worker methodes
-    # TODO: Check that if no voltage is specified, nothing is sent.
-    # TODO: Include programming accuracies
+    # TODO: UI REadback value 
+
     def send_GPIB_voltage(self, voltage=None, output=None):
         # Update the power supply outputs with the specified voltages.
         # If an argument is None, the corresponding value will not be changed
@@ -270,42 +320,60 @@ class HP_6622AWorker(GPIBWorker):
             self.set_i(output,current)
 
 
-    # TODO: check for remote values and warn if control_mode is changing
-    def check_channel_control(self, channel):
-        set_value_voltage = np.round(float(self.GPIB_connection.query('VSET?' + str(channel))), voltage_decimals)
-        set_value_current = np.round(float(self.GPIB_connection.query('ISET?' + str(channel))), current_decimals)
-        out_value_voltage = np.round(float(self.GPIB_connection.query('VOUT?' + str(channel))), voltage_decimals)
-        out_value_current = np.round(float(self.GPIB_connection.query('IOUT?' + str(channel))), current_decimals)
+    # TODO cc or cv ? 
+    # TODO live time refreshing (Future)
+    # def check_channel_control(self, channel): # Not used now
+    #     set_value_voltage = np.round(float(self.GPIB_connection.query('VSET?' + str(channel))), voltage_decimals)
+    #     set_value_current = np.round(float(self.GPIB_connection.query('ISET?' + str(channel))), current_decimals)
+    #     out_value_voltage = np.round(float(self.GPIB_connection.query('VOUT?' + str(channel))), voltage_decimals)
+    #     out_value_current = np.round(float(self.GPIB_connection.query('IOUT?' + str(channel))), current_decimals)
 
-        if out_value_current >= set_value_current:
-            print('Channel %d is in Current Control mode, Out: %.4f, Set: %.4f' % (channel, out_value_current, set_value_current))
-        elif out_value_voltage >= set_value_voltage:
-            print("Channel %d is in Voltage Control mode, Out: %.4f, Set: %.4f" % (channel, out_value_voltage, set_value_voltage))
-        else:
-            print('Output Control Mode is unclear.', out_value_current, set_value_current, out_value_voltage, set_value_voltage)
-        return 
-    def check_remote_values(self):
-        for i in range(self.num_outputs):
-            self.check_channel_control(i + 1)
+    #     if out_value_current >= set_value_current:
+    #         print('Channel %d is in Current Control mode, Out: %.4f, Set: %.4f' % (channel, out_value_current, set_value_current))
+    #     elif out_value_voltage >= set_value_voltage:
+    #         print("Channel %d is in Voltage Control mode, Out: %.4f, Set: %.4f" % (channel, out_value_voltage, set_value_voltage))
+    #     else:
+    #         print('Output Control Mode is unclear.', out_value_current, set_value_current, out_value_voltage, set_value_voltage)
+    #     return 
+
+    def check_status(self,chan):
+        mode = self.get_mode(chan)
+        
+        meanings = ['CP', 'OC', 'UNR', 'OT', 'OV', '-CC', '+CC', 'CV']  # Page 74 OPERATING MANUAL 6622A
+        status = []
+
+        for i in range(8):
+            if mode & (1 << i):  # bitmask where a single bit is set to 1 at the position we're currently checking,
+                                       # and all other bits are 0 (left shift operator <<)
+                status.append(meanings[i])
+        return ' '.join(status)
+
+    def get_readbcks(self):
+        current_output_values = {}
+        for i in range(1, self.num_outputs + 1 ):
+            current_output_values[f'out{i}/voltage'] = np.round(float(self.get_v( i)) , voltage_decimals)
+            current_output_values[f'out{i}/current'] = np.round(float(self.get_i( i)), current_decimals)
+            print(current_output_values) 
+        return current_output_values
 
     def program_manual(self, front_panel_values):
+
         # Get values from the front_panel_settings
         for i in range(self.num_outputs):
             voltage = front_panel_values['out' + str(i + 1) + '/voltage']
             self.send_GPIB_voltage(voltage=voltage, output=i + 1)
 
+
         for i in range(self.num_outputs):
             current = front_panel_values['out' + str(i + 1) + '/current']
             self.send_GPIB_current(current=current, output=i + 1)
-        self.check_remote_values()
-        return {}  # no need to adjust the values. Can add a check_remote_values() here to read current values from power supply
+
+        return {}
 
     def transition_to_buffered(self, device_name, h5_filepath, initial_values, fresh):
         # for remote worker to find correct find path:
         if getattr(self, 'is_remote', False):
             h5_filepath = path_to_local(h5_filepath)
-
-        print(initial_values)
 
         # Get values at first from 'initial_values' and overwrite them afterwards with values given in the experiment script
         dtypes = [('v%d' % (i + 1), np.float32) for i in range(4)] + \
